c9defe9dc916420dcbcd523c51bc15537b3ad1f8
sowmyananjundappa <sowmyananjundappa@users.noreply.github.com>
2017-09-18 18:00:46 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,4 @@
-[Default]
-aws_access_key_id - AKIAJ3FJYP7WKTQVDPIQ
-aws_secret_access_key - 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
+aws_access_key_id = AKIAJ3FJYP7WKTQVDPIQ
+aws_secret_access_key = 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
 
 
