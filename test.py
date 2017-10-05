55f420c493d55108964218c690b4df22508030e3
sowmyananjundappa <sowmyananjundappa@users.noreply.github.com>
2017-10-05 14:22:09 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,2 @@
-aws_access_key_id - AKIAJ3FJYP7WKTQVDPIQ
-aws_secret_access_key - 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
-
-
+aws_access_key_id = AKIAJUJV5PO2GRL7KYRA
+aws_secret_access_key = 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
